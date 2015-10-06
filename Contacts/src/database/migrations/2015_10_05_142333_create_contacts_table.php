d6043516481ef150aa51ffc18a4ff0b1d94e7b43
khaled <rahalkhaled85@gmail.com>
2015-10-06 12:57:06 +0100

    add file migration

---
+++ Contacts/src/database/migrations/2015_10_05_142333_create_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateContactsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contacts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('company');
+			$table->string('email');
+			$table->string('phone')->nullable();
+			$table->text('msg', 65535)->nullable();
+			$table->integer('sort_order')->unsigned()->nullable();
+			$table->boolean('published')->default(1);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contacts');
+	}
+
+}
